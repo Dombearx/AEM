91fae46e7e713fd78cf68b5b3e16e88598f60a8d
Dombearx <domin.dober@gmail.com>
2020-03-22 16:12:14 +0100

    Add greedy surface alg

--- proj.py
+++ proj.py
@@ -29,14 +29,136 @@ def calculateDistance(dist1, dist2):
     return round((math.sqrt(distance)))
 
 
+def showPlot(cords, paths):
+    x = [x for (number, x, y) in cords]
+    y = [y for (number, x, y) in cords]
+    plt.scatter(x, y)
+
+    colors = ["green", "red", "blue", "yellow"]
+
+    for i, (x1, y1) in enumerate(zip(x, y)):
+        plt.annotate(i + 1, (x1, y1))
+
+    for path, color in zip(paths, colors):
+        previousPoint = path[0]
+        for nextPoint in path[1:]:
+            plt.plot([x[previousPoint], x[nextPoint]],
+                     [y[previousPoint], y[nextPoint]], color)
+            previousPoint = nextPoint
+    plt.show()
+
+
+def greedyPath(matrix, firstPoint):
+    summaryDistance = 0
+    matrix = matrix.copy()
+    path = []
+    currentPoint = firstPoint
+
+    for point in matrix:
+        point[firstPoint] = -1
+
+    path.append(currentPoint)
+    for _ in range(0, math.ceil(len(matrix) / 2) - 1):
+        bestDistance = None
+        choosenPoint = None
+        for index, distance in enumerate(matrix[currentPoint]):
+            if(index == currentPoint):
+                continue
+            if(distance == -1):
+                continue
+            if(bestDistance == None or distance < bestDistance):
+                bestDistance = distance
+                choosenPoint = index
+        summaryDistance += bestDistance
+        currentPoint = choosenPoint
+
+        for point in matrix:
+            point[currentPoint] = -1
+
+        path.append(currentPoint)
+
+    path.append(firstPoint)
+    return path, summaryDistance
+
+
+def greedySurfacePath(matrix, firstPoint):
+    summaryDistance = 0
+    matrix = matrix.copy()
+    path = []
+    currentPoint = firstPoint
+
+    distancesToFirstPoint = []
+
+    for point in matrix:
+        distancesToFirstPoint.append(point[firstPoint])
+        point[firstPoint] = -1
+
+    path.append(currentPoint)
+    for _ in range(0, math.ceil(len(matrix) / 2) - 1):
+        bestDistance = None
+
+        tempDistance = None
+
+        choosenPoint = None
+        for index, distance in enumerate(matrix[currentPoint]):
+            if(index == currentPoint):
+                continue
+            if(distance == -1):
+                continue
+
+            calculateDistance = distance + distancesToFirstPoint[index]
+
+            if(bestDistance == None or calculateDistance < bestDistance):
+                bestDistance = calculateDistance
+                tempDistance = distance
+                choosenPoint = index
+
+        summaryDistance += tempDistance
+        currentPoint = choosenPoint
+
+        for point in matrix:
+            point[currentPoint] = -1
+
+        path.append(currentPoint)
+
+    path.append(firstPoint)
+    return path, summaryDistance
+
+
+def bestGreedySurfacePath(matrix):
+    bestDistnace = None
+    for firstPoint in range(0, len(matrix) - 1):
+        foundPath, foundDistance = greedySurfacePath(matrix, firstPoint)
+        if(bestDistnace == None or foundDistance < bestDistnace):
+            bestDistnace = foundDistance
+            path = foundPath
+
+    return path, bestDistnace
+
+
+def bestGreedyPath(matrix):
+    bestDistnace = None
+    for firstPoint in range(0, len(matrix) - 1):
+        foundPath, foundDistance = greedyPath(matrix, firstPoint)
+        if(bestDistnace == None or foundDistance < bestDistnace):
+            bestDistnace = foundDistance
+            path = foundPath
+
+    return path, bestDistnace
+
+
 if __name__ == "__main__":
-    cords = readCords("./problems/kroA100.tsp")
+    cords = readCords("./problems/kroB100.tsp")
     """
     cords = [
         (1, 10, 20),
         (2, 10, 22),
         (3, 15, 1),
-        (4, 6, 12)
+        (4, 6, 12),
+        (5, 14, 22),
+        (6, 15, 2),
+        (7, 15, 3),
+        (8, 16, 4)
     ]
     """
     w = h = len(cords)
@@ -53,20 +175,11 @@ if __name__ == "__main__":
 
     pp.pprint(matrix)
 
-    path = [1, 2, 3, 4, 1]
+    path, summaryDistance = bestGreedyPath(matrix)
 
-    x = [x for (number, x, y) in cords]
-    y = [y for (number, x, y) in cords]
-    plt.scatter(x, y)
+    surfacePath, surfaceSummaryDistance = bestGreedySurfacePath(matrix)
 
-    print(x)
-    print(y)
-    previousPoint = path[0]
-    for nextPoint in path[1:]:
-        print([x[previousPoint - 1], y[previousPoint - 1]])
-        print([x[nextPoint - 1], y[nextPoint - 1]])
-        plt.plot([x[previousPoint - 1], x[nextPoint - 1]],
-                 [y[previousPoint - 1], y[nextPoint - 1]], 'green')
-        previousPoint = nextPoint
-        #plt.plot([10, 10], [20, 22], 'green')
-    plt.show()
+    print(path, summaryDistance)
+    print(surfacePath, surfaceSummaryDistance)
+
+    showPlot(cords, (path, surfacePath))
